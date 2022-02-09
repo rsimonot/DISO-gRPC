882933dd273315dacc041bf06d03bf02b3d05475
rsimonot <rsimonot@magentinehealthcare.com>
2022-02-09 10:51:30 +0100

    first commit

---
+++ server.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+// Even when gRPC will be installed, these directories are wrong (gonna perform a local install)
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include "diso-grpc.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using disogrpc::HandShake;
+using disogrpc::HandShakeInit;
+using disogrpc::HandShakeReply;
+
+// Logic and data behind the server's behavior.
+class HandShakeImpl final : public HandShake::Service {
+    Status performHandShake(ServerContext* context, const HandShakeInit* request, HandShakeReply* reply) override
+    {
+        std::string prefix("HandShake init from ");
+        reply->set_message(prefix + request->name());
+        return Status::OK;
+    }
+};
+
+void RunServer() {
+    std::string server_address("0.0.0.0:5000");
+    HandShakeImpl service;
+
+    grpc::EnableDefaultHealthCheckService(true);
+    grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+    ServerBuilder builder;
+    // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    // Register "service" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *synchronous* service.
+    builder.RegisterService(&service);
+    // Finally assemble the server.
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Wait for the server to shutdown. Note that some other thread must be
+    // responsible for shutting down the server for this call to ever return.
+    server->Wait();
+}
+
+int main(int argc, char** argv) {
+    RunServer();
+
+    return 0;
+}
